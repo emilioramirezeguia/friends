32cc315b854ed0b1b49796b645a13e6aab271b96
Emilio Ramirez <emilio.ramirez@hey.com>
2022-01-31 07:09:42 -0600

    Add form using controlled components

--- pages/add-friend.tsx
+++ pages/add-friend.tsx
@@ -1,15 +1,40 @@
 import type { NextPage } from "next";
+import { useState } from "react";
 import Head from "next/head";
 import styles from "../styles/AddFriend.module.css";
 
 const AddFriend: NextPage = () => {
+  const emptyForm = {
+    name: "",
+    birthday: "",
+    lastContacted: "",
+  };
+
+  const [form, setForm] = useState(emptyForm);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setForm({ ...form, [name]: value });
+  };
+
   return (
     <div className={styles.container}>
       <Head>
         <title>One friend at a time | Friends</title>
       </Head>
       <main className={styles.main}>
-        <h1>Add a friend</h1>
+        <h1>Hey {form.name}</h1>
+        <p>Is this form working? Type in your name and see it display above.</p>
+        <form>
+          <label>
+            Name:
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}></input>
+          </label>
+        </form>
       </main>
     </div>
   );
