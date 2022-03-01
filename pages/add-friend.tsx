2df2732ecb8620f4acb8a75c36bbcff0833b0d6f
Emilio Ramirez <emilio.ramirez@hey.com>
2022-02-28 22:09:08 -0600

    Separate 'name' field and add 'last contacted'

--- pages/add-friend.tsx
+++ pages/add-friend.tsx
@@ -5,7 +5,8 @@ import styles from "../styles/AddFriend.module.css";
 
 const AddFriend: NextPage = () => {
   const emptyForm = {
-    name: "",
+    firstName: "",
+    lastName: "",
     birthday: "",
     lastContacted: "",
   };
@@ -24,25 +25,39 @@ const AddFriend: NextPage = () => {
         <title>One friend at a time | Friends</title>
       </Head>
       <main className={styles.main}>
-        <h1>Hey {form.name}</h1>
+        <h1>Hey {form.firstName}</h1>
         <p>Is this form working? Type in your name and see it display above.</p>
         <form>
-          <label>
-            Name:
-            <input
-              type="text"
-              name="name"
-              value={form.name}
-              onChange={handleChange}></input>
-          </label>
-          <label>
-            Birthday:
-            <input
-              type="date"
-              name="birthday"
-              value={form.birthday}
-              onChange={handleChange}></input>
-          </label>
+          <label>First Name:</label>
+          <input
+            type="text"
+            name="first-name"
+            value={form.firstName}
+            onChange={handleChange}
+          />
+          <label>Last Name: </label>
+          <input
+            type="text"
+            name="last-name"
+            value={form.lastName}
+            onChange={handleChange}
+          />
+
+          <label>Birthday: </label>
+          <input
+            type="date"
+            name="birthday"
+            value={form.birthday}
+            onChange={handleChange}
+          />
+
+          <label>Last contacted</label>
+          <input
+            type="date"
+            name="last-contacted"
+            value={form.lastContacted}
+            onChange={handleChange}
+          />
         </form>
       </main>
     </div>
